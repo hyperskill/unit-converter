2072a01d4fa55f39680919814bfe077190570c42
Alexey Bulenkov <alex@bulenkov.com>
2019-02-25 22:06:19 +0300

    add weight converter + some refactoring

--- src/unitConverter/main.kt
+++ src/unitConverter/main.kt
@@ -2,55 +2,111 @@ package unitConverter
 
 import java.util.*
 
-fun kilometers2meters(kilometers: Double): Double = kilometers * 1000
+fun normalizeMeasure(measure:String): String = when(measure.toLowerCase()) {
+    "m", "meter", "meters" -> "meters"
+    "km", "kilometer", "kilometers" -> "kilometers"
+    "cm", "centimeter", "centimeters" -> "centimeters"
+    "mm", "millimeter", "millimeters" -> "millimeters"
+    "mi", "mile", "miles" -> "miles"
+    "yd", "yard", "yards" -> "yards"
+    "ft", "foot", "feet" -> "feet"
+    "in", "inch", "inches" -> "inches"
+    "g", "gram", "grams" -> "grams"
+    "kg", "kilogram", "kilograms" -> "kilograms"
+    "mg", "milligram", "milligrams" -> "milligrams"
+    "lb", "pound", "pounds" -> "pounds"
+    "oz", "ounce", "ounces" -> "ounces"
 
-fun centimeters2meters(centimeters: Double): Double = centimeters * 0.01
-
-fun millimeters2meters(millimeters: Double): Double = millimeters * 0.001
-
-fun miles2meters(miles: Double): Double = miles * 1609.35
-
-fun yard2meters(yard: Double): Double = yard * 0.9144
-
-fun feet2meters(foot: Double): Double = foot * 0.3048
-
-fun inch2meters(inch: Double): Double = inch * 0.0254
-
-fun formatMeasure(measure:String): String {
-    val measureLower = measure.toLowerCase()
-
-    return when {
-        arrayOf("m", "meter", "meters").contains(measureLower) -> "meter"
-        arrayOf("km", "kilometer", "kilometers").contains(measureLower) -> "kilometers"
-        arrayOf("cm", "centimeter", "centimeters").contains(measureLower) -> "centimeters"
-        arrayOf("mm", "millimeter", "millimeters").contains(measureLower) -> "millimeters"
-        arrayOf("mi", "mile", "miles").contains(measureLower) -> "mile"
-        arrayOf("yd", "yard", "yards").contains(measureLower) -> "yards"
-        arrayOf("ft", "foot", "feet").contains(measureLower) -> "feet"
-        arrayOf("in", "inch", "inches").contains(measureLower) -> "inches"
-        else -> ""
-    }
+    else -> error("can't normalize measure '$measure'")
 }
 
-fun convert2meter(measure: String, amount: Double): Double = when (measure) {
-    "meter" -> amount
-    "kilometers" -> kilometers2meters(amount)
-    "centimeters" -> centimeters2meters(amount)
-    "millimeters" -> millimeters2meters(amount)
-    "mile" -> miles2meters(amount)
-    "yards" -> yard2meters(amount)
-    "feet" -> feet2meters(amount)
-    "inches" -> inch2meters(amount)
-    else -> 0.0
+fun lengthFactor(measure:String): Double = when(measure) {
+    "meters" -> 1.0
+    "kilometers" -> 1000.0
+    "centimeters" -> 0.01
+    "millimeters" -> 0.001
+    "miles" -> 1609.35
+    "yards" -> 0.9144
+    "feet" -> 0.3048
+    "inches" -> 0.0254
+
+    else -> error("unknown length measure")
+}
+
+fun weightFactor(measure:String): Double = when(measure) {
+    "grams" -> 1.0
+    "kilograms" -> 1000.0
+    "milligrams" -> 0.001
+    "pounds" -> 453.592
+    "ounces" -> 28.3495
+
+    else -> error("unknown weight measure")
+}
+
+fun lengthConverter(fromMeasure:String, toMeasure:String, amount:Double = 1.0): Double {
+    val factorFrom = lengthFactor(fromMeasure)
+    val factorTo = lengthFactor(toMeasure)
+
+    return amount * factorFrom / factorTo
+}
+
+fun weightConverter(fromMeasure:String, toMeasure:String, amount:Double = 1.0): Double {
+    val factorFrom = weightFactor(fromMeasure)
+    val factorTo = weightFactor(toMeasure)
+
+    return amount * factorFrom / factorTo
+}
+
+fun isWeightMeasure(measure:String): Boolean = when(measure) {
+    "grams",
+    "kilograms",
+    "milligrams",
+    "pounds",
+    "ounces" -> true
+
+    else -> false
+}
+
+fun isLengthMeasure(measure:String): Boolean = when(measure) {
+    "meters",
+    "kilometers",
+    "centimeters",
+    "millimeters",
+    "miles",
+    "yards",
+    "feet",
+    "inches" -> true
+
+    else -> false
+}
+
+fun calculate(fromMeasure:String, toMeasure:String, amount:Double): Double = when {
+    isWeightMeasure(fromMeasure) -> weightConverter(fromMeasure, toMeasure, amount)
+    isLengthMeasure(fromMeasure) -> lengthConverter(fromMeasure, toMeasure, amount)
+
+    else -> error("Unknown measure type")
 }
 
 fun main(args: Array<String>) {
-    print("Enter a number and a measure of length: ")
-
     val scanner = Scanner(System.`in`)
-    val amount = scanner.nextDouble()
-    val measure = formatMeasure(scanner.next())
-    val inMeters = convert2meter(measure, amount)
 
-    print("$amount $measure is $inMeters meters")
+    while (true) {
+        print("Enter what you want to convert (or Exit): ")
+
+        if (!scanner.hasNextDouble()) {
+            val command = scanner.next().toLowerCase()
+
+            if (command == "exit") {
+                break
+            }
+        }
+
+        val amount = scanner.nextDouble()
+        val fromMeasure = normalizeMeasure(scanner.next())
+        scanner.next()
+        val toMeasure = normalizeMeasure(scanner.next())
+        val result = calculate(fromMeasure, toMeasure, amount)
+
+        println("$amount $fromMeasure is $result $toMeasure")
+    }
 }

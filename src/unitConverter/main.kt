546a43aac1a59cf2f94f57dab39aa04d16a5466e
DjMatesha <dj_matesha@mail.ru>
2019-03-10 20:37:38 +0300

    Update main.kt

--- src/unitConverter/main.kt
+++ src/unitConverter/main.kt
@@ -1,5 +1,13 @@
 package unitConverter
 
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    print("Hello world!")
+    val scanner = Scanner(System.`in`)
+    var n: Int
+    do {
+        print("Enter a number of kilograms: ")
+        n = scanner.nextInt()
+    } while (n < 0)
+    println("$n kilograms is ${n * 1000} grams")
 }
